946c0f94c1b814c63bad3a70ea592a3d1da43fdb
Alok <ay747283@gmail.com>
2026-04-04 16:28:01 +0530

    checkpointer added

--- src/ai_component/graph/graph.py
+++ src/ai_component/graph/graph.py
@@ -13,7 +13,7 @@ from src.exceptions import CustomException
 
 tools_node = ToolNode(tools=[pdf_search_tool, web_tool])
 
-def Workflow():
+def Workflow(checkpointer = None):
     builder = StateGraph(GraphState)
     my_nodes = Nodes()
 
@@ -43,53 +43,4 @@ def Workflow():
         }
     )
 
-    return builder.compile()
-
-
-import asyncio
-from langchain_core.messages import HumanMessage
-async def run_full_workflow_test():
-    print("--- 1. Initializing the Agentic RAG Workflow ---")
-    app = Workflow() 
-    inputs = {
-        "messages": [HumanMessage(content="What does the document say about architecture?")],
-        "session_id": "doc_user_aveek_123"
-    }
-    
-    print(f"\nUser Query: {inputs['messages'][0].content}")
-    print("--- 2. Running Graph (Please wait...) ---\n")
-    result = await app.ainvoke(inputs)
-    
-    print("\n==========================================")
-    print("           FINAL CHAT HISTORY             ")
-    print("==========================================")
-    
-    for msg in result.get("messages", []):
-        msg_type = msg.__class__.__name__
-        
-        if hasattr(msg, 'tool_calls') and msg.tool_calls:
-            print(f"🤖 {msg_type} [ACTION]: Requested Tool -> '{msg.tool_calls[0]['name']}'")
-            print(f"   ↳ Args: {msg.tool_calls[0]['args']}")
-            
-        elif msg_type == "ToolMessage":
-            # Truncating the output so it doesn't flood your console
-            content_preview = msg.content[:150].replace('\n', ' ') + "..." if len(msg.content) > 150 else msg.content
-            print(f"🔧 {msg_type} [RESULT]: {content_preview}")
-            
-        # Normal conversational message (Human, AI text, or Feedback)
-        else:
-            prefix = "👤 Human" if msg_type == "HumanMessage" else "🤖 AI"
-            print(f"{prefix}: {msg.content}")
-        print("-" * 40)
-
-    # 5. Print the Judge's Evaluation
-    print("\n==========================================")
-    print("            JUDGE'S EVALUATION            ")
-    print("==========================================")
-    print(f"Verdict     : {result.get('Judge_response')}")
-    print(f"Reason      : {result.get('Judge_reason')}")
-    print(f"Loops Taken : {result.get('max_loop')} out of your max_tries")
-    print("==========================================\n")
-
-if __name__ == "__main__":
-    asyncio.run(run_full_workflow_test())
+    return builder.compile(checkpointer=checkpointer)
---
+++ src/ai_component/modules/db_memory.py
@@ -0,0 +1,7 @@
+import os
+
+class DatabaseConfig:
+    BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", ".."))
+    DB_PATH = os.path.join(BASE_DIR, "checkpoints.sqlite")
+
+db_config = DatabaseConfig()
